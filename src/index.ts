3412397e1148f9863962839c8a0a0c906ca58ed8
Madan Lal <iammadanlalmali@gmail.com>
2023-09-13 21:37:54 +0530

    project setup

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression());
+app.use(cookieParser())
+app.use(bodyParser.json());
+
+app.use((req, res)=> {
+    console.log(req.method + " " + req.url);
+    res.end()
+})
+
+const server = http.createServer(app);
+
+server.listen(8000, ()=>{
+    console.log("Server is running on localhost:8000");
+})
+
+const MONGODB_URL = "mongodb+srv://iammadanlalmali:nodets@cluster0.pxshgxl.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URL);
+mongoose.connection.on("error", (error: Error)=> console.log(error));
+
