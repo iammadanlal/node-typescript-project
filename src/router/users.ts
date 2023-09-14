4ac81cb3203277927b8024f1c0dca4a3683ce1cc
Madan Lal <iammadanlalmali@gmail.com>
2023-09-14 12:32:48 +0530

    Securing get users api

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 
 import { getAllUsers } from '../controllers/users';
+import { isAuthenticated } from '../middlewares';
 
 export default (router:express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticated, getAllUsers);
     return router;
 }
