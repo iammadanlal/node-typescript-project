da4729293697efdca8fb0a589734480209eddc1a
Madan Lal <iammadanlalmali@gmail.com>
2023-09-14 15:08:47 +0530

    delete api added

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getAllUsers } from '../controllers/users';
+import { getAllUsers, deleteUser } from '../controllers/users';
 import { isAuthenticated } from '../middlewares';
 
 export default (router:express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
+    router.delete('/users/:id', deleteUser)
     return router;
 }
