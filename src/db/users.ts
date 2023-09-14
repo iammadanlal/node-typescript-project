c9794d5fc33a57e85313f29d7b0c0d46e6ea6913
Madan Lal <iammadanlalmali@gmail.com>
2023-09-14 09:51:14 +0530

    Created User methods on model

--- src/db/users.ts
+++ src/db/users.ts
@@ -12,4 +12,12 @@ const userSchema = new mongoose.Schema({
 
 export const User = mongoose.model("User", userSchema);
 
-
+export const getUsers = () => User.find();
+export const getUserByEmail = (email: string) => User.findOne({email});
+export const getUserBySessionToken = (sessionToken: string) => User.findOne({
+    'authentication.sessionToken': sessionToken
+});
+export const getUserById = (id: string) => User.findById(id);
+export const createUser = (values: Record<string, any>) => User.create(values)
+export const deleteUserById = (id: string) => User.findByIdAndDelete(id);
+export const updateUserById = (id: string)=> User.findByIdAndUpdate(id);
