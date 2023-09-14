3880081853e42ea1f6f2991724f0a7553ed0d2af
Madan Lal <iammadanlalmali@gmail.com>
2023-09-14 11:43:21 +0530

    login api added

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -33,3 +33,37 @@ export const register = async (req: express.Request, res: express.Response) => {
         return res.sendStatus(400);
     }
 }
+
+export const login = async(req: express.Request, res: express.Response) => {
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+        if(!user){
+            return res.sendStatus(400);
+        }
+
+        const expectedHash  = authentication(user.authentication.salt, password);
+
+        if(user.authentication.password !== expectedHash){
+            return res.sendStatus(400);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken  = authentication(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie('AUTH', user.authentication.sessionToken, { domain: "localhost", path: '/'});
+
+        return res.status(200).json(user).end();
+    }catch(error){
+        console.error(error);
+        return res.sendStatus(400)
+    }
+}
