8a59e6db42d9d759339410f9d30c61d33b8d2edb
Madan Lal <iammadanlalmali@gmail.com>
2023-09-14 12:17:22 +0530

    get users api added

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getUsers } from 'db/users';
+import { getUsers } from '../db/users';
 
 export const getAllUsers = async(req: express.Request, res: express.Response) => {
     try{
---
+++ src/router/users.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+
+import { getAllUsers } from '../controllers/users';
+
+export default (router:express.Router) => {
+    router.get('/users', getAllUsers);
+    return router;
+}
