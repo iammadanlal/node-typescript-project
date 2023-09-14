78f0358231c4e0719625762286d61db46a9b1fbb
Madan Lal <iammadanlalmali@gmail.com>
2023-09-14 12:10:45 +0530

    create user controller

---
+++ src/controllers/users.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { getUsers } from 'db/users';
+
+export const getAllUsers = async(req: express.Request, res: express.Response) => {
+    try{
+        const users = await getUsers();
+
+        return res.status(200).json(users)
+    }catch(error){
+        console.error(error)
+        return res.sendStatus(400)
+    }
+}
